1abdd8c321461681c0cf4afb872b0ca2926534a9
root <baryshnikov.semen@mail.ru>
2021-01-21 13:15:07 +0300

    begin of work

--- app/config.py
+++ app/config.py
@@ -2,7 +2,8 @@ import os
 
 SECRET_KEY = b'\xeal\xcf]\xfftx\xec\xadb\xd7\xa8S\xd4i\x82'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'develop.db')
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_1096:qwerty123456@std-mysql.ist.mospolytech.ru/std_1096'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'develop.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 UPLOAD_FOLDER = os.path.join(
