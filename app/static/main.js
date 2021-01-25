86012d344589c733aaf1922d844a6a9216828d16
lSenichl <baryshnikov.semen@mail.ru>
2021-01-25 23:54:33 +0300

    some fixes

--- app/static/main.js
+++ app/static/main.js
@@ -6,6 +6,22 @@ $('#delete-movie-modal').on('show.bs.modal', function (event) {
     this.querySelector('#movie-name').textContent = movieName;
 })
 
+function imagePreviewHandler(event) {
+    if (event.target.files && event.target.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            let img = document.querySelector('.background-preview > img');
+            img.src = e.target.result;
+            if (img.classList.contains('d-none')) {
+                let label = document.querySelector('.background-preview > label');
+                label.classList.add('d-none');
+                img.classList.remove('d-none');
+            }
+        }
+        reader.readAsDataURL(event.target.files[0]);
+    }
+}
+
 const TOOLBAR_ITEMS = [
     "bold", "italic", "heading", "|", 
     "quote", "ordered-list", "unordered-list", "|",
@@ -15,6 +31,10 @@ const TOOLBAR_ITEMS = [
 ]
 
 window.onload = function() {
+    let background_img_field = document.getElementById('background_img');
+    if (background_img_field) {
+        background_img_field.onchange = imagePreviewHandler;
+    }
     if (document.getElementById('text-content')) {
         let easyMDE = new EasyMDE({
             element: document.getElementById('text-content'),
